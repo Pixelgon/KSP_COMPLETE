a1a4e7519bf891096c592943badeac20b05de47a
pixelgon <pixelgon4@gmail.com>
2022-07-22 16:51:09 +0200

    KSP4 pridano

---
+++ KSP4.py
@@ -0,0 +1,30 @@
+# -----------------------------------------------------------
+# KSP4
+# Vytvoril Matej Matejka
+# -----------------------------------------------------------
+
+origin = open("01.in", "r")
+nA = int(origin.readline())
+sA = []
+sB = []
+
+for i in range(nA):
+    sA.append(int(origin.readline()))
+sA.sort()
+
+nB = int(origin.readline())
+for i in range(nB):
+    sB.append(int(origin.readline()))
+sB.sort()
+origin.close()
+
+ndvojic = 0
+for x in sA:
+    for y in sB:
+        if x == y:
+            ndvojic += 1
+
+output = open("01.out", "w")
+output.write(str(ndvojic))
+output.close()
+exit()
