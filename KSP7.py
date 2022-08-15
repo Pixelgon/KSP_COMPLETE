de06f4e0a4150c56265908a8e15713632f14df2f
pixelgon <pixelgon4@gmail.com>
2022-08-15 02:14:09 +0200

    KSP7 pridano

---
+++ KSP7.py
@@ -0,0 +1,28 @@
+# -----------------------------------------------------------
+# KSP6
+# Vytvoril Matej Matejka
+# -----------------------------------------------------------
+
+# Nacetem file cislem
+origin = open("01.in", "r")
+numA = int(origin.readline())
+numB = int(origin.readline())
+origin.close()
+
+
+def gcd(na, nb):
+    if nb == 0:
+        return na
+    else:
+        return gcd(nb, na % nb)
+
+
+def lcm(x, y):
+    return int(x * y / gcd(x, y))
+
+
+output = open("01.out", "w")
+output.write(str(gcd(numA, numB)) + "\n")
+output.write(str(lcm(numA, numB)))
+output.close()
+exit()
